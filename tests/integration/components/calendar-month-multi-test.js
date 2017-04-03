605b4384ada252c957b37d4054022aee6b0355cf
Ryan Rishi <ryan@ryanrishi.com>
2017-04-03 11:25:51 -0700

    started adding tests

--- tests/integration/components/calendar-month-multi-test.js
+++ tests/integration/components/calendar-month-multi-test.js
@@ -6,18 +6,43 @@ moduleForComponent('calendar-month-multi', 'Integration | Component | calendar m
 });
 
 test('it works when the start date is pushed to the first week of the next month', function(assert) {
-  assert.ok(true);
-});
-
-test('it does not show the previous button if the start date is at the end of a previous month', function(assert) {
-  let component = this.subject();
-
   let startDate = '03/29/2017';
   let endDate = '04/16/2017';
 
-  component.setProperties({ startDate, endDate });
+  this.setProperties({ startDate, endDate });
 
-  this.render(hbs`{{calendar-month-multi}}`);
+  this.render(hbs`{{calendar-month-multi
+                    startDate=startDate
+                    endDate=endDate}}`);
 
-  assert.equal(this.$('.previous').text().trim(), '');
+  assert.equal(this.$('.previous').hasClass('hidden'), true, 'it does not show the previous button');
+  assert.equal(this.$('.calendar-month').length, 1, 'it only renders one month');
+});
+
+test('it only shows two months if the start date is pushed to the first week of the next month', function(assert) {
+  // this is kinda a confusing test title
+
+  let startDate = '07/31/2017';
+  let endDate = '09/14/2017';
+
+  this.setProperties({ startDate, endDate });
+
+  this.render(hbs`{{calendar-month-multi
+                    startDate=startDate
+                    endDate=endDate}}`);
+
+  assert.equal(this.$('.calendar-month').length, 2, 'it only renders two months');
+});
+
+test('it shows two months', function(assert) {
+  let startDate = '04/14/2017';
+  let endDate = '05/14/2017';
+
+  this.setProperties({ startDate, endDate });
+
+  this.render(hbs`{{calendar-month-multi
+                    startDate=startDate
+                    endDate=endDate}}`);
+
+  assert.equal(this.$('.calendar-month').length, 2, 'it renders two months');
 });
--- tests/integration/components/calendar-month-test.js
+++ tests/integration/components/calendar-month-test.js
@@ -10,16 +10,11 @@ test('it renders', function(assert) {
   // Set any properties with this.set('myProperty', 'value');
   // Handle any actions with this.on('myAction', function(val) { ... });
 
-  this.render(hbs`{{calendar-month}}`);
+  this.render(hbs`{{calendar-month
+    month=2
+    year=2017
+    showMonthName=true
+    showYear=false}}`);
 
-  assert.equal(this.$().text().trim(), '');
-
-  // Template block usage:
-  this.render(hbs`
-    {{#calendar-month}}
-      template block text
-    {{/calendar-month}}
-  `);
-
-  assert.equal(this.$().text().trim(), 'template block text');
+  assert.equal(this.$('thead .month-name').text().trim(), 'March');
 });
